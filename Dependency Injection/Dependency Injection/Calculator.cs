fd6334e3d0a0e167a99a48f4276e8273355bc1bb
Gokulgosfsfdfdfdfdgdg <Gokul@DEgSKTOP-GQMUBVT>
2019-10-26 21:46:19 +0530

    chnages in substratction vlue

---
+++ Dependency Injection/Dependency Injection/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        
+       private IOperation opp;
+        public Calculator(IOperation op)
+        {
+            this.opp = op;
+        }
+       public int  doaction(int a,int b)
+        {
+            return this.opp.Execute(a, b);
+
+        }
+
+    }
+}
---
+++ Dependency Injection/Dependency Injection/Substraction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Substraction : IOperation
+    {
+        public int Execute(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
